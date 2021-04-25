b0000cee5f1d79183e96ec1497c8e38a40eb1b5f
Liam Sho <conan_creeper@outlook.com>
2021-04-25 14:28:46 +0800

    Improve Exp-6 code quality

--- Exp6_C8051F020_MCU_Basic_Configuration/Exp6_C8051F020_MCU_Basic_Configuration.c
+++ Exp6_C8051F020_MCU_Basic_Configuration/Exp6_C8051F020_MCU_Basic_Configuration.c
@@ -2,21 +2,21 @@
 
 sbit P21 = P2 ^ 1;
 
-//外部12MHz晶振初始化
+// 外部晶振初始化
 void Oscillator_Init()
 {
     int i = 0;
-    OSCXCN = 0x67;             //配置成外部晶体振荡器
-    for (i = 0; i < 3000; i++) //等待1ms以上，至少循环3000次
+    OSCXCN = 0x67;             // 配置成外部晶体振荡器，0x67 为 12Mhz，0x77 为 6Mhz
+    for (i = 0; i < 3000; i++) // 等待1ms以上，至少循环3000次
     {
     }
-    while ((OSCXCN & 0x80) == 0) // 有效标志位为1跳出循环
+    while ((OSCXCN & 0x80) == 0) // 有效标志位为1跳出循环，即 XTLVLD = 1
     {
     }
-    OSCICN = 0x0c; //切换到外部振荡器
+    OSCICN = 0x08; // 切换到外部振荡器
 }
 
-//延迟函数
+// 延迟函数
 void delay(unsigned int t)
 {
     unsigned int j;
@@ -31,20 +31,21 @@ void delay(unsigned int t)
 //I/O口初始化程序
 void Port_IO_Init()
 {
-    XBR0 = 0x02;    //SPI
-    XBR1 = 0x1E;    //T0，T1，INT0，INT1
-    XBR2 = 0x40;    //XBR2.6=1  交叉开关使能
-    P1MDOUT = 0x1F; //P1.5,P1.6,P1.7漏极开路
-    P1 = 0xE0;      //P1.5，P1.6，P1.7逻辑1；
-    P1MDIN = 0x1F;  //P1.5，P1.6，P1.7模拟输入
+    XBR0 = 0x02;    // SPI | XBR0 = 0000 0010
+    XBR1 = 0x1E;    // T0(1)，T1(3)，INT0(2)，INT1(4) | XBR1 = 0001 1110
+    XBR2 = 0x40;    // 交叉开关使能 | XBR2.6 = 1
+    P1MDOUT = 0x1F; // P1.5,P1.6,P1.7 输出模式配置 漏极开路 | P1MDOUT = 0001 1111
+    P1 = 0xE0;      // P1.5，P1.6，P1.7 置1 | P1 = 1110 0000
+    P1MDIN = 0x1F;  // P1.5，P1.6，P1.7 输入模式配置 模拟输入 | P1MDIN = 0001 1111
 }
 
-//禁用看门狗程序
+// 禁用看门狗
 void Disable_Watchdog()
 {
-    EA = 0;
-    WDTCN = 0xDE;
+    EA = 0;       // 禁止所有中断，下两条语句须在4时钟周期内，防止打断
+    WDTCN = 0xDE; // 关闭看门狗
     WDTCN = 0xAD;
+    EA = 1; // 恢复中断
 }
 
 void main()
@@ -53,7 +54,7 @@ void main()
     Disable_Watchdog();
     Port_IO_Init();
 
-    P2MDOUT = 0x02; //推挽方式输出
+    P2MDOUT = 0x02; // P2.1 推挽方式输出
     P21 = 0;
     while (1)
     {
